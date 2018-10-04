bc65fd64489a076b9d1dfffdf3d97e4b6c3f1ccf
Iain Winter <iainwinter1@gmail.com>
2018-10-03 22:08:45 -0400

    Woo

---
+++ TheLocal/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheLocal.Models;
+
+namespace TheLocal.Controllers {
+    public class TestController : Controller {
+        public IActionResult Test() {
+            var name = new TestModel() { Name = "This is a test!!" };
+            return View(name);
+        }
+    }
+}
