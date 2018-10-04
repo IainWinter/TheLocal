9cc005a35bacf169a7351107bf9d19b83adf3d58
Iain Winter <iainwinter1@gmail.com>
2018-10-04 15:13:22 -0400

    Created a test form thing. Moved started code to Default.zip

--- TheLocal/Controllers/HomeController.cs
+++ TheLocal/Controllers/HomeController.cs
@@ -1,36 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using TheLocal.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace TheLocal.Controllers {
     public class HomeController : Controller {
-        public IActionResult Index() {
+        [HttpGet]
+        public ViewResult Index() {
             return View();
         }
 
-        public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact() {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpPost]
+        public ContentResult Index(string first, string last) {
+            return Content($"Hello {first}, {last}!");
         }
     }
 }
