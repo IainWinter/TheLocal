51f84ea72ce36b5ac67a5238badfe1413387629e
Iain Winter <iainwinter1@gmail.com>
2018-10-22 12:56:55 -0400

    Tarjs

--- TheLocal/Controllers/HomeController.cs
+++ TheLocal/Controllers/HomeController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TheLocal.Models;
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace TheLocal.Controllers {
     public class HomeController : Controller {
         [HttpGet]
         public ViewResult Index() {
-            return View();
+            IList<Name> list = new List<Name>();
+
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder {
+                DataSource = "avidata.cymuktbsfffe.us-east-2.rds.amazonaws.com",
+                UserID = "root",
+                Password = "rootroot",
+                Pooling = false
+            };
+
+            SqlConnection connection = new SqlConnection(sb.ConnectionString);
+            SqlCommand command = new SqlCommand("SELECT * FROM users;", connection);
+            connection.Open();
+
+            SqlDataReader reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+            string first = reader.GetString(1);
+            string last = reader.GetString(2);
+
+            reader.Close();
+            list.Add(new Name() { First = first, Last = last });
+            return View(list);
         }
 
         [HttpPost]
